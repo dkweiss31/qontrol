61bbb6df95746977e1a3ce6e26e0354657a0a815
Danny <dkweiss@u.northwestern.edu>
2024-09-06 07:06:21 -0400

    new init structure

--- optamiqs/__init__.py
+++ optamiqs/__init__.py
@@ -1,29 +1,18 @@
-from .fidelity import *
-from .file_io import *
-from .grape import *
-from .options import *
-from .pulse_optimizer import *
-from .cost import *
-
-
-__all__ = [
-    'grape',
-    'GRAPEOptions',
-    'PulseOptimizer',
-    'save_and_print',
-    'append_to_h5',
-    'write_to_h5',
-    'generate_file_path',
-    'extract_info_from_h5',
-    'infidelity_coherent',
-    'infidelity_incoherent',
-    'incoherent_infidelity',
-    'coherent_infidelity',
-    'all_cardinal_states',
-    "IncoherentInfidelity",
-    "CoherentInfidelity",
-    "ForbiddenStates",
-    "ControlArea",
-    "ControlNorm",
-    "CustumCost",
-]
+from .fidelity import all_cardinal_states as all_cardinal_states
+from .file_io import (
+    save_and_print as save_and_print,
+    append_to_h5 as append_to_h5,
+    write_to_h5 as write_to_h5,
+    generate_file_path as generate_file_path,
+    extract_info_from_h5 as extract_info_from_h5
+)
+from .grape import grape as grape
+from .options import GRAPEOptions as GRAPEOptions
+from .hamiltonian_time import hamiltonian_time_updater as hamiltonian_time_updater
+from .cost import (
+    incoherent_infidelity as incoherent_infidelity,
+    coherent_infidelity as coherent_infidelity,
+    forbidden_states as forbidden_states,
+    control_area as control_area,
+    control_norm as control_norm,
+)
