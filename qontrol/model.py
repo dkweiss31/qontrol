40face85994edb7fa3a231e610f92788299b77e3
Danny <dkweiss@u.northwestern.edu>
2024-12-03 20:26:07 -0500

    move H_labels to options

--- qontrol/model.py
+++ qontrol/model.py
@@ -24,7 +24,6 @@ def sesolve_model(
     tsave_or_function: ArrayLike | callable,
     *,
     exp_ops: list[ArrayLike] | None = None,
-    H_labels: list | None = None,
 ) -> SESolveModel:
     r"""Instantiate sesolve model.
 
@@ -103,9 +102,7 @@ def sesolve_model(
     H_function, psi0, tsave_or_function, exp_ops = _initialize_model(
         H_function, psi0, tsave_or_function, exp_ops
     )
-    return SESolveModel(
-        H_function, psi0, tsave_or_function, exp_ops=exp_ops, H_labels=H_labels
-    )
+    return SESolveModel(H_function, psi0, tsave_or_function, exp_ops=exp_ops)
 
 
 def mesolve_model(
@@ -115,7 +112,6 @@ def mesolve_model(
     tsave_or_function: ArrayLike | callable,
     *,
     exp_ops: list[ArrayLike] | None = None,
-    H_labels: list | None = None,
 ) -> MESolveModel:
     r"""Instantiate mesolve model.
 
@@ -160,12 +156,7 @@ def mesolve_model(
     )
     jump_ops = [_astimearray(L) for L in jump_ops]
     return MESolveModel(
-        H_function,
-        rho0,
-        tsave_function,
-        exp_ops=exp_ops,
-        H_labels=H_labels,
-        jump_ops=jump_ops,
+        H_function, rho0, tsave_function, exp_ops=exp_ops, jump_ops=jump_ops
     )
 
 
@@ -256,7 +247,6 @@ class Model(eqx.Module):
     initial_states: Array
     tsave_function: callable
     exp_ops: Array | None
-    H_labels: list | None
 
     def __call__(
         self,
