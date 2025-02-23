b0ed1d7fc6e3072601e376a0c9943ab25839d0cc
Danny <dkweiss@u.northwestern.edu>
2025-02-23 14:48:49 -0500

    task format

--- tests/test_costs.py
+++ tests/test_costs.py
@@ -1,13 +1,16 @@
 import dynamiqs as dq
-import qontrol as ql
 from jax.random import PRNGKey
+
+import qontrol as ql
 
 
 def test_symmetric_forbidden_states():
     dim = 5
     n_states = 4
     n_forbid = 3
-    forbidden_states_qarray = dq.random.randket(PRNGKey(31), (n_states, n_forbid, dim, 1))
+    forbidden_states_qarray = dq.random.randket(
+        PRNGKey(31), (n_states, n_forbid, dim, 1)
+    )
     cost = ql.forbidden_states(forbidden_states_qarray)
     assert cost.forbidden_states.shape == (n_states, 1, n_forbid, dim, 1)
 
@@ -27,8 +30,10 @@ def test_ragged_forbidden_states():
 
 
 def test_mul():
-    costs = [ql.incoherent_infidelity([dq.basis(2, 0)]),
-             ql.custom_cost(lambda _r, _H, _p: 0.3, 1.0, 0.0)]
+    costs = [
+        ql.incoherent_infidelity([dq.basis(2, 0)]),
+        ql.custom_cost(lambda _r, _H, _p: 0.3, 1.0, 0.0),
+    ]
     for cost in costs:
         new_cost = 2.0 * cost
         assert new_cost.cost_multiplier == 2.0
